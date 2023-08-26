0bc1e24bd2f36af8a0ad82d17d3fa9c0da56ef9a
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:53:25 +0530

    Update and rename Q-26 to C26.cpp

--- C26.cpp
+++ C26.cpp
@@ -1,3 +1,7 @@
+/*In any language program mostly syntax error occurs due to unbalancing delimiter such as
+(),{},[]. Write C++ program using stack to check whether given expression is well
+parenthesized or not.*/
+
 #include <iostream>
 using namespace std;
 #define size 10
