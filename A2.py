2a97441cf2c800ceb68e665f047a453624ce4393
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:51:14 +0530

    Update and rename Q-2 to A2.py

--- A2.py
+++ A2.py
@@ -1,3 +1,11 @@
+'''Write a Python program to store marks scored in subject “Fundamental of Data
+Structure” by N students in the class. Write functions to compute following:
+a) The average score of class
+b) Highest score and lowest score of class
+c) Count of students who were absent for the test
+d) Display mark with highest frequency'''
+
+
 print('SUB : Fundamental of Data Structure')
 n=int(input("Enter no. of students in a class : "))
 marks=[]
