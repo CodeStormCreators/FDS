dd2d16c83206c4afa401186642d8e16f0e8cbb29
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:52:20 +0530

    Update and rename Q-22 to C22.cpp

--- C22.cpp
+++ C22.cpp
@@ -1,3 +1,11 @@
+/*Second year Computer Engineering class, set A of students like Vanilla Ice-cream and set
+B of students like butterscotch ice-cream. Write C++ program to store two sets using
+linked list. compute and displaya) Set of students who like both vanilla and butterscotch
+b) Set of students who like either vanilla or butterscotch or not both
+c) Number of students who like neither vanilla nor butterscotch
+*/
+
+
 #include <iostream>
 using namespace std;
 
