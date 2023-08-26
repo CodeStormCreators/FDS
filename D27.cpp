9795030e9719a9967700e3429a00a8b31307ff37
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:54:49 +0530

    Update and rename Q-27 to D27.cpp

--- D27.cpp
+++ D27.cpp
@@ -1,3 +1,11 @@
+/* Implement C++ program for expression conversion as infix to postfix and its evaluation
+using stack based on given conditions:
+1. Operands and operator, both must be single character.
+2. Input Postfix expression must be in a desired format.
+3. Only '+', '-', '*' and '/ ' operators are expected.
+*/
+
+
 #include <iostream>
 using namespace std;
 #define arr_size 20
