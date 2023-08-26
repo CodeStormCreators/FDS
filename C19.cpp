8f9367de5845d84e1bc143a4ce1d714c1fb8dc27
CodeStormCreators <142803048+CodeStormCreators@users.noreply.github.com>
2023-08-27 00:50:20 +0530

    Update and rename Q-19 to C19.cpp

--- C19.cpp
+++ C19.cpp
@@ -1,3 +1,16 @@
+/*Department of Computer Engineering has student's club named 'Pinnacle Club'. Students
+of second, third and final year of department can be granted membership on request.
+Similarly one may cancel the membership of club. First node is reserved for president of
+club and last node is reserved for secretary of club. Write C++ program to maintain club
+member‘s information using singly linked list. Store student PRN and Name. Write
+functions to:
+a) Add and delete the members as well as president or even secretary.
+b) Compute total number of members of club
+c) Display members
+d) Two linked lists exists for two divisions. Concatenate two lists.
+*/
+
+
 #include <iostream>
 using namespace std;
 
